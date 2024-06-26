555a4b93b1c80cd42b220a3948ea0685d05f6ac7
halosatrio <63773815+halosatrio@users.noreply.github.com>
2024-06-27 00:37:16 +0700

    feat: api get transaction

--- routes/transaction.ts
+++ routes/transaction.ts
@@ -4,6 +4,7 @@ import { zValidator } from "@hono/zod-validator";
 import type { JwtPayloadType } from "@lib/common";
 import { jwtMiddleware } from "@utils/jwtMiddleware";
 import { transactionValidationSchema as transactionValidation } from "@utils/validationSchema";
+import { eq } from "drizzle-orm";
 import { Hono } from "hono";
 
 // TODO:
@@ -12,6 +13,23 @@ export const transactionRoutes = new Hono();
 transactionRoutes.use(jwtMiddleware);
 
 transactionRoutes
+  .get("/", async (c) => {
+    // TODO:
+    // query params limit, date: start & end, category,
+    // do i need to add is_active to transaction (?)
+    const jwtPayload: JwtPayloadType = c.get("jwtPayload");
+
+    const transactions = await db
+      .select()
+      .from(transactionsTable)
+      .where(eq(transactionsTable.user_id, jwtPayload.sub));
+
+    return c.json({
+      status: 200,
+      message: "Success!",
+      data: transactions,
+    });
+  })
   .post(
     "/create",
     zValidator("json", transactionValidation, (result, c) => {
@@ -36,7 +54,7 @@ transactionRoutes
         .values({
           user_id: jwtPayload.sub,
           type: body.type,
-          amount: body.amount.toString(),
+          amount: body.amount,
           category: body.category,
           date: body.date, // Convert date string to Date object
           note: body.note,
@@ -53,22 +71,18 @@ transactionRoutes
       });
     }
   )
-  .get("/", async (c) => {
-    const jwtPayload: JwtPayloadType = c.get("jwtPayload");
-    return c.json({
-      status: 200,
-      message: "Success!",
-      data: jwtPayload,
-    });
-  })
-  .put("/transaction", async (c) => {
+  .put("/", async (c) => {
+    // TODO:
+    // add endpoint /:id. PUT by transaction id
     return c.json({
       status: 200,
       message: "Success!",
       data: "Success update transaction",
     });
   })
-  .delete("/transaction", async (c) => {
+  .delete("/", async (c) => {
+    // TODO:
+    // add endpoint /:id. DELETE by transaction id
     return c.json({
       status: 200,
       message: "Success!",
