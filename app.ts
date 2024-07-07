ac3c62fc6c53bd59d3bcb8694eba55eed405c436
halosatrio <63773815+halosatrio@users.noreply.github.com>
2024-07-07 12:08:43 +0700

    feat: handle cors localhost

--- app.ts
+++ app.ts
@@ -2,10 +2,18 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { authRoutes } from "@routes/auth";
 import { transactionRoutes } from "@routes/transaction";
+import { cors } from "hono/cors";
 
 const app = new Hono();
 
 app.use("*", logger());
+app.use("*", cors());
+app.use(
+  "*",
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
 
 app
   .basePath("/v1")
