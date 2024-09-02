22061a9288970c531a50eb23a3564a8df2dbcfd9
halosatrio <63773815+halosatrio@users.noreply.github.com>
2024-09-02 13:42:32 +0700

    feat: report quarter

--- routes/report.ts
+++ routes/report.ts
@@ -5,7 +5,7 @@ import type { JwtPayloadType } from "@lib/common";
 import { jwtMiddleware } from "@utils/jwtMiddleware";
 import { quarterReportQuerySchema } from "@utils/validationSchema";
 import dayjs from "dayjs";
-import { and, eq, inArray, sql } from "drizzle-orm";
+import { and, between, eq, inArray, sql } from "drizzle-orm";
 import { Hono } from "hono";
 
 export const reportRoutes = new Hono();
@@ -44,35 +44,68 @@ reportRoutes.get(
       );
     }
 
-    // TODO: define date range based on year and q from query
-    // let date_start = dayjs
+    const quarterMonth: { [key: string]: string[] } = {
+      "1": ["january", "february", "march"],
+      "2": ["april", "may", "june"],
+      "3": ["july", "august", "september"],
+      "4": ["october", "november", "december"],
+    };
 
-    const existingQuarterMonthOne = await db
-      .select({
-        category: transactionsTable.category,
-        amount: sql<number>`cast(sum(${transactionsTable.amount}) as int)`,
-      })
-      .from(transactionsTable)
-      .where(
-        and(
-          eq(transactionsTable.user_id, jwtPayload.sub),
-          eq(transactionsTable.is_active, true),
-          inArray(transactionsTable.category, [
-            "makan",
-            "cafe",
-            "utils",
-            "errand",
-            "bensin",
-            "olahraga",
-          ])
+    const getFirstDate = (month: number) =>
+      dayjs(`${year} ${quarterMonth[q][month]}`)
+        .startOf("month")
+        .format("YYYY-MM-DD");
+    const getLastDate = (month: number) =>
+      dayjs(`${year} ${quarterMonth[q][month]}`)
+        .startOf("month")
+        .format("YYYY-MM-DD");
+
+    // TODO: define date range based on year and q from query
+    let month1: string[] = [getFirstDate(0), getLastDate(0)];
+    let month2: string[] = [getFirstDate(1), getLastDate(1)];
+    let month3: string[] = [getFirstDate(2), getLastDate(2)];
+    const essentials = [
+      "makan",
+      "cafe",
+      "utils",
+      "errand",
+      "bensin",
+      "olahraga",
+    ];
+
+    const getQuarterQuery = (date1: string, date2: string) =>
+      db
+        .select({
+          category: transactionsTable.category,
+          amount: sql<number>`cast(sum(${transactionsTable.amount}) as int)`,
+        })
+        .from(transactionsTable)
+        .where(
+          and(
+            eq(transactionsTable.user_id, jwtPayload.sub),
+            eq(transactionsTable.is_active, true),
+            inArray(transactionsTable.category, essentials),
+            date1 && date2
+              ? between(transactionsTable.date, date1, date2)
+              : undefined
+          )
         )
-      )
-      .groupBy(transactionsTable.category);
+        .groupBy(transactionsTable.category);
+
+    const quarterMonth1 = await getQuarterQuery(month1[0], month1[1]);
+    const quarterMonth2 = await getQuarterQuery(month2[0], month2[1]);
+    const quarterMonth3 = await getQuarterQuery(month3[0], month3[1]);
+    const quarterTotal = await getQuarterQuery(month1[0], month3[1]);
 
     return c.json({
       status: 200,
       message: "Success!",
-      data: existingQuarterMonthOne,
+      data: {
+        quarterMonth1,
+        quarterMonth2,
+        quarterMonth3,
+        quarterTotal,
+      },
     });
   }
 );
