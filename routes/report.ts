16455a2c5a6a6de28c0b7d94fad8f1b68bac28e7
halosatrio <63773815+halosatrio@users.noreply.github.com>
2024-09-02 14:17:41 +0700

    chore: refactor

--- routes/report.ts
+++ routes/report.ts
@@ -15,6 +15,30 @@ const QUARTER_MONTH: { [key: string]: string[] } = {
   "4": ["october", "november", "december"],
 };
 
+// extract/refactor recurring functions
+const getFirstDate = (year: string, q: string, month: number) =>
+  dayjs(`${year} ${QUARTER_MONTH[q][month]}`).startOf("month").format("YYYY-MM-DD");
+const getLastDate = (year: string, q: string, month: number) =>
+  dayjs(`${year} ${QUARTER_MONTH[q][month]}`).endOf("month").format("YYYY-MM-DD");
+
+const getQuarterQuery = (jwt: JwtPayloadType, date1: string, date2: string, category: string[]) =>
+  db
+    .select({
+      category: transactionsTable.category,
+      amount: sql<number>`cast(sum(${transactionsTable.amount}) as int)`,
+    })
+    .from(transactionsTable)
+    .where(
+      and(
+        eq(transactionsTable.user_id, jwt.sub),
+        eq(transactionsTable.is_active, true),
+        inArray(transactionsTable.category, category),
+        date1 && date2 ? between(transactionsTable.date, date1, date2) : undefined
+      )
+    )
+    .groupBy(transactionsTable.category);
+
+// start Route Functions
 export const reportRoutes = new Hono();
 reportRoutes.use(jwtMiddleware);
 
@@ -39,63 +63,22 @@ reportRoutes.get(
     // catch error if query is not number
     const { year, q } = c.req.query();
     if (!parseInt(year)) {
-      return c.json(
-        { status: 500, message: "Query 'year' is not a number!" },
-        500
-      );
+      return c.json({ status: 500, message: "Query 'year' is not a number!" }, 500);
     }
     if (!parseInt(q)) {
-      return c.json(
-        { status: 500, message: "Query 'q' is not a number!" },
-        500
-      );
+      return c.json({ status: 500, message: "Query 'q' is not a number!" }, 500);
     }
 
-    const getFirstDate = (month: number) =>
-      dayjs(`${year} ${QUARTER_MONTH[q][month]}`)
-        .startOf("month")
-        .format("YYYY-MM-DD");
-    const getLastDate = (month: number) =>
-      dayjs(`${year} ${QUARTER_MONTH[q][month]}`)
-        .endOf("month")
-        .format("YYYY-MM-DD");
-
     // TODO: define date range based on year and q from query
-    let month1: string[] = [getFirstDate(0), getLastDate(0)];
-    let month2: string[] = [getFirstDate(1), getLastDate(1)];
-    let month3: string[] = [getFirstDate(2), getLastDate(2)];
-    const essentials = [
-      "makan",
-      "cafe",
-      "utils",
-      "errand",
-      "bensin",
-      "olahraga",
-    ];
+    let month1: string[] = [getFirstDate(year, q, 0), getLastDate(year, q, 0)];
+    let month2: string[] = [getFirstDate(year, q, 1), getLastDate(year, q, 1)];
+    let month3: string[] = [getFirstDate(year, q, 2), getLastDate(year, q, 2)];
+    const essentials = ["makan", "cafe", "utils", "errand", "bensin", "olahraga"];
 
-    const getQuarterQuery = (date1: string, date2: string) =>
-      db
-        .select({
-          category: transactionsTable.category,
-          amount: sql<number>`cast(sum(${transactionsTable.amount}) as int)`,
-        })
-        .from(transactionsTable)
-        .where(
-          and(
-            eq(transactionsTable.user_id, jwtPayload.sub),
-            eq(transactionsTable.is_active, true),
-            inArray(transactionsTable.category, essentials),
-            date1 && date2
-              ? between(transactionsTable.date, date1, date2)
-              : undefined
-          )
-        )
-        .groupBy(transactionsTable.category);
-
-    const quarterMonth1 = await getQuarterQuery(month1[0], month1[1]);
-    const quarterMonth2 = await getQuarterQuery(month2[0], month2[1]);
-    const quarterMonth3 = await getQuarterQuery(month3[0], month3[1]);
-    const quarterTotal = await getQuarterQuery(month1[0], month3[1]);
+    const quarterMonth1 = await getQuarterQuery(jwtPayload, month1[0], month1[1], essentials);
+    const quarterMonth2 = await getQuarterQuery(jwtPayload, month2[0], month2[1], essentials);
+    const quarterMonth3 = await getQuarterQuery(jwtPayload, month3[0], month3[1], essentials);
+    const quarterTotal = await getQuarterQuery(jwtPayload, month1[0], month3[1], essentials);
 
     return c.json({
       status: 200,
@@ -131,65 +114,22 @@ reportRoutes.get(
     // catch error if query is not number
     const { year, q } = c.req.query();
     if (!parseInt(year)) {
-      return c.json(
-        { status: 500, message: "Query 'year' is not a number!" },
-        500
-      );
+      return c.json({ status: 500, message: "Query 'year' is not a number!" }, 500);
     }
     if (!parseInt(q)) {
-      return c.json(
-        { status: 500, message: "Query 'q' is not a number!" },
-        500
-      );
+      return c.json({ status: 500, message: "Query 'q' is not a number!" }, 500);
     }
 
-    const getFirstDate = (month: number) =>
-      dayjs(`${year} ${QUARTER_MONTH[q][month]}`)
-        .startOf("month")
-        .format("YYYY-MM-DD");
-    const getLastDate = (month: number) =>
-      dayjs(`${year} ${QUARTER_MONTH[q][month]}`)
-        .endOf("month")
-        .format("YYYY-MM-DD");
-
-    console.log(getFirstDate(1), getLastDate(1));
-
     // TODO: define date range based on year and q from query
-    let month1: string[] = [getFirstDate(0), getLastDate(0)];
-    let month2: string[] = [getFirstDate(1), getLastDate(1)];
-    let month3: string[] = [getFirstDate(2), getLastDate(2)];
-    const nonEssentials = [
-      "family",
-      "misc",
-      "transport",
-      "traveling",
-      "date",
-      "healthcare",
-    ];
+    let month1: string[] = [getFirstDate(year, q, 0), getLastDate(year, q, 0)];
+    let month2: string[] = [getFirstDate(year, q, 1), getLastDate(year, q, 1)];
+    let month3: string[] = [getFirstDate(year, q, 2), getLastDate(year, q, 2)];
+    const nonEssentials = ["family", "misc", "transport", "traveling", "date", "healthcare"];
 
-    const getQuarterQuery = (date1: string, date2: string) =>
-      db
-        .select({
-          category: transactionsTable.category,
-          amount: sql<number>`cast(sum(${transactionsTable.amount}) as int)`,
-        })
-        .from(transactionsTable)
-        .where(
-          and(
-            eq(transactionsTable.user_id, jwtPayload.sub),
-            eq(transactionsTable.is_active, true),
-            inArray(transactionsTable.category, nonEssentials),
-            date1 && date2
-              ? between(transactionsTable.date, date1, date2)
-              : undefined
-          )
-        )
-        .groupBy(transactionsTable.category);
-
-    const quarterMonth1 = await getQuarterQuery(month1[0], month1[1]);
-    const quarterMonth2 = await getQuarterQuery(month2[0], month2[1]);
-    const quarterMonth3 = await getQuarterQuery(month3[0], month3[1]);
-    const quarterTotal = await getQuarterQuery(month1[0], month3[1]);
+    const quarterMonth1 = await getQuarterQuery(jwtPayload, month1[0], month1[1], nonEssentials);
+    const quarterMonth2 = await getQuarterQuery(jwtPayload, month2[0], month2[1], nonEssentials);
+    const quarterMonth3 = await getQuarterQuery(jwtPayload, month3[0], month3[1], nonEssentials);
+    const quarterTotal = await getQuarterQuery(jwtPayload, month1[0], month3[1], nonEssentials);
 
     return c.json({
       status: 200,
@@ -225,33 +165,11 @@ reportRoutes.get(
     // catch error if query is not number
     const { year, q } = c.req.query();
     if (!parseInt(year)) {
-      return c.json(
-        { status: 500, message: "Query 'year' is not a number!" },
-        500
-      );
+      return c.json({ status: 500, message: "Query 'year' is not a number!" }, 500);
     }
     if (!parseInt(q)) {
-      return c.json(
-        { status: 500, message: "Query 'q' is not a number!" },
-        500
-      );
+      return c.json({ status: 500, message: "Query 'q' is not a number!" }, 500);
     }
-
-    const getFirstDate = (month: number) =>
-      dayjs(`${year} ${QUARTER_MONTH[q][month]}`)
-        .startOf("month")
-        .format("YYYY-MM-DD");
-    const getLastDate = (month: number) =>
-      dayjs(`${year} ${QUARTER_MONTH[q][month]}`)
-        .endOf("month")
-        .format("YYYY-MM-DD");
-
-    console.log(getFirstDate(1), getLastDate(1));
-
-    // TODO: define date range based on year and q from query
-    let month1: string[] = [getFirstDate(0), getLastDate(0)];
-    let month2: string[] = [getFirstDate(1), getLastDate(1)];
-    let month3: string[] = [getFirstDate(2), getLastDate(2)];
 
     const getQuarterQuery = (date1: string, date2: string) =>
       db
@@ -265,12 +183,15 @@ reportRoutes.get(
             eq(transactionsTable.user_id, jwtPayload.sub),
             eq(transactionsTable.is_active, true),
             eq(transactionsTable.category, "belanja"),
-            date1 && date2
-              ? between(transactionsTable.date, date1, date2)
-              : undefined
+            date1 && date2 ? between(transactionsTable.date, date1, date2) : undefined
           )
         )
         .groupBy(transactionsTable.category);
+
+    // TODO: define date range based on year and q from query
+    let month1: string[] = [getFirstDate(year, q, 0), getLastDate(year, q, 0)];
+    let month2: string[] = [getFirstDate(year, q, 1), getLastDate(year, q, 1)];
+    let month3: string[] = [getFirstDate(year, q, 2), getLastDate(year, q, 2)];
 
     const quarterMonth1 = await getQuarterQuery(month1[0], month1[1]);
     const quarterMonth2 = await getQuarterQuery(month2[0], month2[1]);
