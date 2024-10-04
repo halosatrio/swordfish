854b9c0382c7540633d6f1211ddd1654a6785700
bayu-labs <labs@bayubit.com>
2024-10-04 03:41:17 +0100

    fix: add 7 hours to logger for GMT+7, not using timezone

--- utils/customLogger.ts
+++ utils/customLogger.ts
@@ -1,6 +1,6 @@
 import dayjs from "dayjs";
 
 export const customLogger = (message: string, ...rest: string[]) => {
-  let timestamp = dayjs().format("YYYY-MM-DD - HH:mm:ss");
+  let timestamp = dayjs().add(7, 'hour').format("YYYY-MM-DD - HH:mm:ss");
   console.log(timestamp, message, ...rest);
 };
